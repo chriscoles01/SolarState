c6f56ce582bdc0e9be1234d8722f3658a3697f3d
Christopher Coles <cbc1g16@soton.ac.uk>
2020-02-22 21:04:57 +0100

    update to include zip finding

--- frontend/app.js
+++ frontend/app.js
@@ -9,22 +9,22 @@ import ControlPanel from './control-panel';
 // Set your mapbox token here
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2hyaXNjb2xlczAxIiwiYSI6ImNrNnhqaDF3dzBhNjMzZW8waHpnMzN5ZWsifQ.mLeEly0rwEBCNiffXh_0tg'; // eslint-disable-line
 
-// export const COLOR_SCALE = scaleThreshold()
-//   .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-//   .range([
-//     [65, 182, 196],
-//     [127, 205, 187],
-//     [199, 233, 180],
-//     [237, 248, 177],
-//     // zero
-//     [255, 255, 204],
-//     [255, 237, 160],
-//     [254, 217, 118],
-//     [254, 178, 76],
-//     [253, 141, 60],
-//     [252, 78, 42],
+export const COLOR_SCALE = scaleThreshold()
+  .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  .range([
+    [65, 182, 196],
+    [127, 205, 187],
+    [199, 233, 180],
+    [237, 248, 177],
+    // zero
+    [255, 255, 204],
+    [255, 237, 160],
+    [254, 217, 118],
+    [254, 178, 76],
+    [253, 141, 60],
+    [252, 78, 42],
     
-//   ]);
+  ]);
 
 const INITIAL_VIEW_STATE = {
   latitude: 41.650623,
@@ -54,57 +54,88 @@ export default class App extends Component {
 
     this.state = {
       hoveredObject: null,
-      Month: "ANNUAL"
+      choice: "ANNUAL",
+      modifier: 1,
+       text:" Solar Potential",
+      INITIAL_VIEW_STATE: {
+        latitude: 41.650623,
+        longitude: -102.693757,
+        zoom: 3,
+        maxZoom: 16,
+        pitch: 10,
+        bearing: 0
+      }
+      
     };
     this._onHover = this._onHover.bind(this);
     this._renderTooltip = this._renderTooltip.bind(this);
-    this.renderLayers = this.renderLayers.bind(this);
 
     const lightingEffect = new LightingEffect({ambientLight, dirLight});
     lightingEffect.shadowColor = [0, 0, 0, 0.5];
     this._effects = [lightingEffect];
-    this.onChangeMonth = this.onChangeMonth.bind(this);
+    this.onChangechoice = this.onChangechoice.bind(this);
+    this.onSelectM2 = this.onSelectM2.bind(this);
+
+    this.onSelectRooftop = this.onSelectRooftop.bind(this);
+    this.zipChange = this.zipChange.bind(this);
 
   }
-  onChangeMonth(vMonth){
-    console.log(vMonth)
-    this.setState({ Month: vMonth});
+  onChangechoice(choice){
+    this.setState({ choice: choice});
 
     
   }
+  zipChange(zip){
+    const data = require('../backend/result.json');
+    console.log(zip)
+   data.features.forEach((feature) => {
+     if(zip == "None"){
+      this.setState({
+        
+        viewState: undefined
+     })}
+      if(feature.properties.zip == zip){
+        var coordx = feature.properties.X_COORD
+        var coordy = feature.properties.Y_COORD
+        this.setState({
+        
+          viewState: {
+            latitude: coordy,
+            longitude: coordx,
+            zoom: 10,
+            maxZoom: 16,
+            pitch: 0,
+            bearing: 0
+          }})
+          console.log(zip)
 
+      }
+    }
+      );
+  }
+
+  onSelectM2(checked){
+    if(checked){
+      this.setState({choice: "norm_potential", modifier: 100000, text:"/ M^2"})
+    } else {
+      this.setState({choice: "ANNUAL", modifier: 1, text:" Solar Potential"})
+    }
+    
+  }
+  onSelectRooftop(checked){
+    if(checked){
+      this.setState({choice: "abs_potential", modifier: 1, text:" total possible on roof"})
+    } else {
+      this.setState({choice: "ANNUAL", modifier: 1, text:" Solar Potential"})
+    }
+  }
   _onHover({x, y, object}) {
     this.setState({x, y, hoveredObject: object});
   }
-  onStyleLoad = (map, e) => {
-    this.setState( {map} );
-  }
-  componentWillUpdate(nextProps, nextState) {
-    const { map, Month } = nextState;
-    if (map) {
-      map.getSource('geojson')(Month);
-    }
-  }
-  
-  _renderLayers(month) {
-    const COLOR_SCALE = scaleThreshold()
-  .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-  .range([
-    [65, 182, 196],
-    [127, 205, 187],
-    [199, 233, 180],
-    [237, 248, 177],
-    // zero
-    [255, 255, 204],
-    [255, 237, 160],
-    [254, 217, 118],
-    [254, 178, 76],
-    [253, 141, 60],
-    [252, 78, 42],
-    
-  ]);
-    const data = require('../data/solardata.json');
-    console.log(month)
+
+  _renderLayers(choice) {
+    const data = require('../backend/result.json');
+    // this.setState({data:data})
     return [
       // only needed when using shadows - a plane for shadows to drop on
       new PolygonLayer({
@@ -122,43 +153,18 @@ export default class App extends Component {
         filled: true,
         extruded: true,
         wireframe: true,
-        // getElevation: f => Math.sqrt(f.properties[this.state.Month]) * 100000 ,
-        getFillColor: f => COLOR_SCALE(f.properties[month]),
+        // getElevation: f => Math.sqrt(f.properties[this.state.choice]) * 100000 ,
+        getFillColor: f => COLOR_SCALE(f.properties[choice] / this.state.modifier),
         getLineColor: [255, 255, 255],
         pickable: true,
-        onHover: this._onHover
+        onHover: this._onHover,
+        updateTriggers: {
+          getFillColor: [this.state.choice]
+      }
       })
     ];
   }
-  renderLayers(state){
-    const {mapStyle = 'mapbox://styles/mapbox/light-v9'} = this.props;
 
-return(
-  <DeckGL
-    onStyleLoad={this.onStyleLoad}
-        layers={this._renderLayers(state.Month)}
-        // effects={this._effects}
-        initialViewState={INITIAL_VIEW_STATE}
-        controller={true}
-      >
-        <StaticMap
-          reuseMaps
-          mapStyle={mapStyle}
-          preventStyleDiffing={true}
-          mapboxApiAccessToken={MAPBOX_TOKEN}
-        />
-      <ControlPanel 
-      onChangeMonth = {this.onChangeMonth}
-      Month= {state.Month}/>
-        {this._renderTooltip}
-      </DeckGL>
-)
-  }
-  componentWillUpdate(nextProps, nextState) {
-    return (
-        this.render()
-    )
-  }
   _renderTooltip() {
     const {x, y, hoveredObject} = this.state;
     return (
@@ -168,11 +174,13 @@ return(
             <b>Average Solar Radiation</b>
           </div>
           <div>
-            <div>{hoveredObject.properties.AREA_1} / size</div>
+            <div>{hoveredObject.properties.AREA_1} KM size </div>
             <div>
-              {hoveredObject.properties[this.state.Month]} KWh
-{              this.state.Month
-}            </div>
+              {hoveredObject.properties[this.state.choice]} KWh {this.state.text}
+            </div>
+            <div>
+            ZIP: {hoveredObject.properties.zip} 
+            </div>
           </div>
          </div>
      
@@ -181,9 +189,31 @@ return(
   }
 
   render() {
+    const {mapStyle = 'mapbox://styles/mapbox/light-v9'} = this.props;
 
     return (
-      this.renderLayers(this.state)
+      <DeckGL
+        layers={this._renderLayers(this.state.choice)}
+        // effects={this._effects}
+        initialViewState={this.state.INITIAL_VIEW_STATE}
+        controller={true}
+        viewState={this.state.viewState}
+
+      >
+        <StaticMap
+          reuseMaps
+          mapStyle={mapStyle}
+          preventStyleDiffing={true}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        />
+      <ControlPanel 
+      onChangechoice = {this.onChangechoice}
+      choice= {this.state.choice}
+      onSelectM2={this.onSelectM2}
+      onSelectRooftop={this.onSelectRooftop}
+      zipChange={this.zipChange}/>
+        {this._renderTooltip}
+      </DeckGL>
     );
   }
 }
--- frontend/control-panel.js
+++ frontend/control-panel.js
@@ -2,8 +2,8 @@ import React, {PureComponent} from 'react';
 
 export default class ControlPanel extends PureComponent {
   render() {
-    var {onChangeMonth, Month} = this.props;
-    this.setState({ Month: Month});
+    var {onChangeMonth, Month, onSelectM2, onSelectRooftop, zipChange} = this.props;
+    this.setState({ Month: Month, checkm2:false, checkroof:false});
 
     const _onChangeMonth = evt => {
       const bMonth = evt.target.value;
@@ -11,8 +11,32 @@ export default class ControlPanel extends PureComponent {
       onChangeMonth(bMonth);
     };
 
-    
+    const _onChangeM2 = evt => {
+      var checked = evt.target.checked
+      onSelectM2(checked)
+      // if(checked){
+      // this.setState({checkroof:true})
+      // }else{
+      //   this.setState({checkroof:true})
 
+      // }
+
+    }
+    
+    const _onChangeRoof = evt => {
+      var checked = evt.target.checked
+      onSelectRooftop(checked)
+      
+    }
+    const _enterZip = evt => {
+      var zip = evt.target.value
+      if(zip.length == 5){
+        zipChange(zip)
+      }
+      if(zip.length == 0){
+        zipChange("None")
+      }
+    }
     return (
       <div className="control-panel">
         <h3>SolarState</h3>
@@ -40,6 +64,17 @@ export default class ControlPanel extends PureComponent {
         <option>DECEMBER</option>
         <option>ANNUAL</option>
       </select>
+      <br/>
+      <input type="checkbox" id="m2" name="m2" value="KWh per m2" onChange={_onChangeM2} />
+      See Rooftop Potential Kwh per m2
+      <br/>
+      <input type="checkbox" id="solar" name="solar" value="See Rooftop Potential" onChange={_onChangeRoof} />
+      See Rooftop Potential Kwh Total
+      <br/>
+      Zip Code:  
+      <input type="number" id="zip" name="zip" onChange={_enterZip}/>
+      <br/>
+      Clear zip to move map
         </div>
         <hr />
        
