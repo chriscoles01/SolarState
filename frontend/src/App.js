21ca80681618555c73bc354700893bd3f6b60fb9
Christopher Coles <cbc1g16@soton.ac.uk>
2020-02-22 14:32:53 +0100

    data json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SimpleMap from './components/SimpleMap.js'
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL, {Source, Layer} from 'react-map-gl';
+import ControlPanel from './control-panel';
+import {json as requestJson} from 'd3-request';
+import {heatmapLayer} from './map-style';
 
-function App() {
-  const latitude = 34.350196
-  const longitude = -116.248543
-  const coords = {latitude, longitude}
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learntest
-        </a>
-      </header>
-      <body>
-        <SimpleMap location={coords} emergency_location = {coords}/> 
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2hyaXNjb2xlczAxIiwiYSI6ImNrNnhqaDF3dzBhNjMzZW8waHpnMzN5ZWsifQ.mLeEly0rwEBCNiffXh_0tg'; // Set your mapbox token here
 
-      </body>
-    </div>
-  );
+function filterFeaturesByDay(featureCollection, time) {
+  const date = new Date(time);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const features = featureCollection.features.filter(feature => {
+    const featureDate = new Date(feature.properties.time);
+    return (
+      featureDate.getFullYear() === year &&
+      featureDate.getMonth() === month &&
+      featureDate.getDate() === day
+    );
+  });
+  return {type: 'FeatureCollection', features};
 }
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    const current = new Date().getTime();
+
+    this.state = {
+      viewport: {
+        latitude: 40,
+        longitude: -100,
+        zoom: 3,
+        bearing: 0,
+        pitch: 0
+      },
+      allDay: true,
+      startTime: current,
+      endTime: current,
+      selectedTime: current,
+      earthquakes: null
+    };
+
+    this._handleChangeDay = this._handleChangeDay.bind(this);
+    this._handleChangeAllDay = this._handleChangeAllDay.bind(this);
+  }
+
+  componentDidMount() {
+    var response = require('../../data/solardata.json'); //(with path)
+    const features = response.features;
+          const endTime = features[0].properties.time;
+          const startTime = features[features.length - 1].properties.time;
+
+          this.setState({
+            data: response,
+            earthquakes: response,
+            endTime,
+            startTime,
+            selectedTime: endTime
+          });
+    
+  }
+
+  _onViewportChange = viewport => this.setState({viewport});
+
+  _handleChangeDay = time => {
+    this.setState({selectedTime: time});
+    if (this.state.earthquakes) {
+      this.setState({data: filterFeaturesByDay(this.state.earthquakes, time)});
+    }
+  };
+
+  _handleChangeAllDay = allDay => {
+    this.setState({allDay});
+    if (this.state.earthquakes) {
+      this.setState({
+        data: allDay
+          ? this.state.earthquakes
+          : filterFeaturesByDay(this.state.earthquakes, this.state.selectedTime)
+      });
+    }
+  };
+
+  render() {
+    const {viewport, data, allDay, selectedTime, startTime, endTime} = this.state;
+
+    return (
+      <div style={{height: '100%', position: 'relative'}}>
+        <MapGL
+          {...viewport}
+          width="100%"
+          height="100%"
+          mapStyle="mapbox://styles/mapbox/light-v10"
+          onViewportChange={this._onViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        >
+          {data && (
+            <Source type="geojson" data={data}>
+              <Layer {...heatmapLayer} />
+            </Source>
+          )}
+        </MapGL>
+        <ControlPanel
+          containerComponent={this.props.containerComponent}
+          startTime={startTime}
+          endTime={endTime}
+          selectedTime={selectedTime}
+          allDay={allDay}
+          onChangeDay={this._handleChangeDay}
+          onChangeAllDay={this._handleChangeAllDay}
+        />
+      </div>
+    );
+  }
+}
+
+export function renderToDom(container) {
+  render(<App />, container);
+}
