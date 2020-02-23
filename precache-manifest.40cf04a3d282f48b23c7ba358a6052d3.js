3c20b85dce3d97673ff46e13095c5145d89d1011
Christopher Coles <cbc1g16@soton.ac.uk>
2020-02-23 19:07:29 +0100

    Updates

---
+++ precache-manifest.40cf04a3d282f48b23c7ba358a6052d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ccc2ceb67cbd3bc39348b711d6686b",
+    "url": "/SolarState/index.html"
+  },
+  {
+    "revision": "5d89a00fcfcdb1e59659",
+    "url": "/SolarState/static/css/main.e1e122d4.chunk.css"
+  },
+  {
+    "revision": "d672f0e75d9c0804bce8",
+    "url": "/SolarState/static/js/2.3fdffb2f.chunk.js"
+  },
+  {
+    "revision": "a0a3063ed0e7e344881a79d4bea9f313",
+    "url": "/SolarState/static/js/2.3fdffb2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d89a00fcfcdb1e59659",
+    "url": "/SolarState/static/js/main.34ed059c.chunk.js"
+  },
+  {
+    "revision": "9fa49fa42cce54d81f70",
+    "url": "/SolarState/static/js/runtime-main.6a1517ff.js"
+  }
+]);
